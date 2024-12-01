72f5c301aa820827b411e6b6f9f0de6cec7364a1
jjr <jamesroddie@icloud.com>
2024-11-30 19:58:52 -0600

    update socials

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -36,10 +36,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://x.com/jamesjroddie"
+            href="https://bsky.app/profile/jroddie.bsky.social"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">x</p>
+            <p className="ml-2 h-7">bluesky</p>
           </a>
         </li>
 
