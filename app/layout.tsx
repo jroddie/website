e599c483be89a9ebd1cf9ad710f1ce4f671b8780
jjr <jamesroddie@icloud.com>
2024-12-06 10:59:57 -0600

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,7 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
           <Footer />
+          <Analytics />
           <SpeedInsights />
         </main>
       </body>
